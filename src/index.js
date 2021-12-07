7df6decb74cb6cb440a01e015807e13bc95fc264
Tomas Lin <thekingofthemountain@gmail.com>
2021-12-08 00:53:11 +0800

    add redux-persist and setup

--- src/index.js
+++ src/index.js
@@ -2,25 +2,26 @@ import React from "react";
 import "utils/firebase";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import { SnackbarProvider } from "notistack";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@material-ui/core/styles";
-import configureStore from "./store/configureStore";
+import store, { persistor } from "./store/configureStore";
 import registerServiceWorker from "./registerServiceWorker";
 import App from "./App";
 import theme from "theme";
 
-const store = configureStore();
-
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <SnackbarProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SnackbarProvider>
-    </ThemeProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <SnackbarProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </SnackbarProvider>
+      </ThemeProvider>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,7 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 import {
@@ -7,6 +9,12 @@ import {
   stopFetchingMiddleware,
 } from 'middlewares/fetchingMiddleware';
 import {globalMessageMiddleware} from 'middlewares/globalMessageMiddleware';
+
+const persistConfig = {
+  storage,
+  key: '!&@#$%^drcleaneradmin',
+  blacklist: ['setting', 'cleaner', 'user', 'order'],
+};
 
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware({});
@@ -19,23 +27,30 @@ const configureStore = () => {
       stopFetchingMiddleware,
       globalMessageMiddleware,
     ];
-
-  const store = {
-    ...createStore(
-      rootReducer,
-      composeEnhancers(applyMiddleware(...middlewares))
-    ),
-    runSaga: sagaMiddleware.run(rootSaga)
-  };
-
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  const prevanteStore = createStore(
+    persistedReducer,
+    composeEnhancers(applyMiddleware(...middlewares)),
+  );
+  const persistor = persistStore(prevanteStore);
+  
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers/index', () => {
       const nextRootReducer = require('../reducers/index');
-      store.replaceReducer(nextRootReducer);
+      prevanteStore.replaceReducer(nextRootReducer);
     });
   }
-  return store;
+  return {
+    persistor,
+    store: {
+      ...prevanteStore,
+      runSaga: sagaMiddleware.run(rootSaga),
+    },
+  };
 };
 
-export default configureStore;
+const {store, persistor} = configureStore();
+
+export {persistor};
+export default store;
