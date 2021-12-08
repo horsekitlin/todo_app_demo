718f6b853701bd4bf670d0efc7433f465ac92f9f
Tomas Lin <thekingofthemountain@gmail.com>
2021-12-08 18:01:21 +0800

    新增與編輯任務後馬上更新任務列表

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,15 +1,30 @@
-import initialState from './initialState';
-import types from 'constants/actionTypes';
+import initialState from "./initialState";
+import types from "constants/actionTypes";
+import cloneDeep from "lodash/cloneDeep";
 
+const updateTaskSuccess = (task, payload) => {
+  const index = task.list.findIndex((task) => task.id === payload.id);
+  const nextTaskList = cloneDeep(task.list);
+  nextTaskList[index] = payload;
+  return { ...task, list: nextTaskList };
+};
 export default function routeReducer(
   task = initialState.task,
-  {type, payload},
+  { type, payload }
 ) {
   switch (type) {
+    case types.UPDATE_TASK_SUCCESS:
+      return updateTaskSuccess(task, payload);
+    case types.ADD_TASK_SUCCESS:
+      return { ...task, list: [payload, ...task.list] };
     case types.GET_TASKS_SUCCESS:
       return { ...task, list: payload };
     case types.GET_TASKS:
     case types.GET_TASKS_ERROR:
+    case types.ADD_TASK:
+    case types.ADD_TASK_ERROR:
+    case types.UPDATE_TASK:
+    case types.UPDATE_TASK_ERROR:
     default:
       return task;
   }
