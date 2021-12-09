787f308927a398eedd97a8f8b91eef0b36e294b8
Tomas Lin <thekingofthemountain@gmail.com>
2021-12-09 14:01:40 +0800

    註冊 Facebook Goolge 增加錯誤訊息

--- src/screens/SignUpScreen/index.js
+++ src/screens/SignUpScreen/index.js
@@ -1,82 +1,13 @@
-import React from "react";
-import queryString from 'query-string';
-import { useNavigate } from "react-router-dom";
-import {
-  Box,  
-  Button,
-  makeStyles,
-} from "@material-ui/core";
-import Page from "components/Page";
-import {
-  FacebookLoginButton,
-  GoogleLoginButton,
-} from "react-social-login-buttons";
-import {
-  getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  FacebookAuthProvider,
-} from "firebase/auth";
+import { sendMessageAction } from 'actions/globalAreaActions';
+import {connect} from 'react-redux';
+import SignUpScreen from './view';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.dark,
-    height: "100%",
-    paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3),
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = (dispatch) => ({
+  handleSendMessage: (payload) => {
+    dispatch(sendMessageAction(payload.status, payload.text));
   },
-}));
+});
 
-const handleSubmitByThirdParty = (provider, navigate) => async () => {
-  try {
-    const auth = getAuth();
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    const payload = {
-      googleId: user.uid,
-      email: user.email,
-      name: user.displayName,
-    };
-    const search = queryString.stringify(payload);
-    navigate({
-      pathname: '/signupform',
-      search: `?${search}`,
-    });
-  } catch (error) {
-    console.log(
-      "🚀 ~ file: index.js ~ line 121 ~ onClick={ ~ error",
-      error
-    );
-  }
-};
-
-const SignUpScreen = () => {
-  const classes = useStyles();
-  const navigate = useNavigate();
-
-  return (
-    <Page className={classes.root} title="Register">
-      <Box
-        display="flex"
-        flexDirection="column"
-        height="100%"
-        alignItems='center'
-        justifyContent="center"
-      >
-        <Box width='30%'> 
-          <Button  fullWidth color='primary' variant='contained' style={{ height: 50 }} onClick={() => navigate({pathname: '/signupform'})}>
-            <p>Email Login</p>
-          </Button>
-        </Box>
-        <Box width='30%'>
-          <FacebookLoginButton text='Registe With Facebook' onClick={handleSubmitByThirdParty(new FacebookAuthProvider(), navigate)}  />
-        </Box>
-        <Box width='30%'>
-          <GoogleLoginButton text='Registe With Google' onClick={handleSubmitByThirdParty(new GoogleAuthProvider(), navigate)} />
-        </Box>
-      </Box>
-    </Page>
-  );
-};
-
-export default SignUpScreen;
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpScreen);
---
+++ src/screens/SignUpScreen/view.js
@@ -0,0 +1,79 @@
+import React from "react";
+import queryString from 'query-string';
+import { useNavigate } from "react-router-dom";
+import {
+  Box,  
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import Page from "components/Page";
+import {
+  FacebookLoginButton,
+  GoogleLoginButton,
+} from "react-social-login-buttons";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+} from "firebase/auth";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    height: "100%",
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3),
+  },
+}));
+
+const handleSubmitByThirdParty = (provider, navigate, handleSendMessage) => async () => {
+  try {
+    const auth = getAuth();
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    const payload = {
+      googleId: user.uid,
+      email: user.email,
+      name: user.displayName,
+    };
+    const search = queryString.stringify(payload);
+    navigate({
+      pathname: '/signupform',
+      search: `?${search}`,
+    });
+  } catch (error) {
+    handleSendMessage({status: 'error', text: '登入三方錯誤'});
+  }
+};
+
+const SignUpScreen = ({handleSendMessage}) => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  return (
+    <Page className={classes.root} title="Register">
+      <Box
+        display="flex"
+        flexDirection="column"
+        height="100%"
+        alignItems='center'
+        justifyContent="center"
+      >
+        <Box width='30%'> 
+          <Button  fullWidth color='primary' variant='contained' style={{ height: 50 }} onClick={() => navigate({pathname: '/signupform'})}>
+            <p>Email Login</p>
+          </Button>
+        </Box>
+        <Box width='30%'>
+          <FacebookLoginButton text='Registe With Facebook' onClick={handleSubmitByThirdParty(new FacebookAuthProvider(), navigate, handleSendMessage)}  />
+        </Box>
+        <Box width='30%'>
+          <GoogleLoginButton text='Registe With Google' onClick={handleSubmitByThirdParty(new GoogleAuthProvider(), navigate, handleSendMessage)} />
+        </Box>
+      </Box>
+    </Page>
+  );
+};
+
+export default SignUpScreen;
